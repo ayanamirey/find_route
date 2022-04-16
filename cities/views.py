a8933b4d95349bc40c825ab59a80dab61aa17c81
Sanjar <sanjar.ravshanbekov@gmail.com>
2022-04-17 00:54:43 +0500

    Creating a form based on the Form and ModelForm classes. Using Bootstrap to Improve Form Display

--- cities/views.py
+++ cities/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 
+from cities.forms import HtmlForm, CityForm
 from cities.models import City
 
 __all__ = (
@@ -9,14 +10,21 @@ __all__ = (
 
 
 def home(request, pk=None):
-    if pk:
-        # city = City.objects.filter(pk=pk).first()
-        city = get_object_or_404(City, pk=pk)
-        context = {'object': city}
-        return render(request, 'cities/detail.html', context)
+    # if pk:
+    #     # city = City.objects.filter(pk=pk).first()
+    #     city = get_object_or_404(City, pk=pk)
+    #     context = {'object': city}
+    #     return render(request, 'cities/detail.html', context)
 
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+
+    form = CityForm()
     qs = City.objects.all()
-    context = {'objects_list': qs}
+    context = {'objects_list': qs, 'form': form}
     return render(request, 'cities/home.html', context)
 
 
