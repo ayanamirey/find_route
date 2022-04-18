a2e92d4f6f337ee59f9555e4a81336024c6de9ec
Jamoliddin Bakhriddinov <milsolve@gmail.com>
2022-04-18 13:52:43 +0500

    Список поездов (формирование, отображение)

--- trains/models.py
+++ trains/models.py
@@ -1,9 +1,10 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from cities.models import City
 
 
-class Trains(models.Model):
+class Train(models.Model):
     name = models.CharField(max_length=50, unique=True, verbose_name='Номер поезда')
     travel_time = models.PositiveSmallIntegerField(verbose_name='Время в пути')
     from_city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='from_city_set',
@@ -18,3 +19,15 @@ class Trains(models.Model):
         verbose_name = 'Поезд'
         verbose_name_plural = 'Поезда'
         ordering = ['travel_time']
+
+    def clean(self):
+        if self.from_city == self.to_city:
+            raise ValidationError('Изменить город прибытия')
+        qs = Train.objects.filter(from_city=self.from_city, to_city=self.to_city,
+                                  travel_time=self.travel_time).exclude(pk=self.pk)
+        if qs.exists():
+            raise ValidationError('Измените время в пути')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
--- trains/views.py
+++ trains/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, UpdateView, ListView
+# from trains.forms import HtmlForm, TrainForm
+from trains.models import Train
 
-# Create your views here.
+__all__ = (
+    'home', 'TrainListView',
+    # 'TrainDetailView', 'TrainCreateView', 'TrainUpdateView', 'post_delete', 
+)
+
+
+def home(request, pk=None):
+    qs = Train.objects.all()
+    lst = Paginator(qs, 5)
+    page_number = request.GET.get('page')
+    page_obj = lst.get_page(page_number)
+    context = {'page_obj': page_obj}
+    return render(request, 'trains/home.html', context)
+
+
+class TrainListView(ListView):
+    paginate_by = 5
+    model = Train
+    template_name = 'trains/home.html'
+
+
+# class TrainDetailView(DetailView):
+#     queryset = Train.objects.all()
+#     template_name = 'trains/detail.html'
+# 
+# 
+# class TrainCreateView(SuccessMessageMixin, CreateView):
+#     model = Train
+#     form_class = TrainForm
+#     template_name = 'trains/create.html'
+#     success_url = reverse_lazy('trains:home')
+#     success_message = 'Город успешно создан'
+# 
+# 
+# class TrainUpdateView(SuccessMessageMixin, UpdateView):
+#     model = Train
+#     form_class = TrainForm
+#     template_name = 'trains/update.html'
+#     success_url = reverse_lazy('trains:home')
+#     success_message = 'Город успешно отредактирован'
+# 
+# 
+# def post_delete(request, pk=None):
+#     post = get_object_or_404(Train, pk=pk).delete()
+#     messages.success(request, 'Город успешно удален')
+#     return redirect('trains:home')
