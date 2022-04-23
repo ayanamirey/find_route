3184500508ec7fcf188be833d929d04192befb0c
Sanjar <sanjar.ravshanbekov@gmail.com>
2022-04-23 20:15:52 +0500

    Формирование скрытых полей в форме

--- routes/forms.py
+++ routes/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from cities.models import City
+from trains.models import Train
 
 
 class RouteForm(forms.Form):
@@ -17,3 +18,16 @@ class RouteForm(forms.Form):
     traveling_time = forms.IntegerField(label='Время в пути',
                                         widget=forms.NumberInput(
                                             attrs={'class': 'form-control', 'placeholder': 'Время в пути'}))
+
+
+class RouteModelForm(forms.ModelForm):
+    name = forms.CharField(label='Название маршрута')
+    from_city = forms.ModelChoiceField(
+        queryset=City.objects.all(), widget=forms.HiddenInput())
+    to_city = forms.ModelChoiceField(
+        queryset=City.objects.all(), widget=forms.HiddenInput())
+    trains = forms.ModelMultipleChoiceField(
+        queryset=Train.objects.all(),
+        required=False, widget=forms.SelectMultiple(
+            attrs={'class': 'form-control d-none'}))
+    travel_times = forms.IntegerField(widget=forms.HiddenInput())
